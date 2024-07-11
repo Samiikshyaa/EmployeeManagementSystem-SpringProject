728fb5b25fca195f9e5779c9591070a1a8111ab4
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-07-11 13:07:19 +0545

    EmployeeList

--- src/main/java/com/bway/springproject/controller/EmployeeController.java
+++ src/main/java/com/bway/springproject/controller/EmployeeController.java
@@ -1,23 +1,39 @@
 package com.bway.springproject.controller;
 
 import com.bway.springproject.model.Employee;
+import com.bway.springproject.service.DepartmentService;
 import com.bway.springproject.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EmployeeController {
+    @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    private DepartmentService departmentService;
 
     @GetMapping("/employee")
-    public String getEmployee(){
+    public String getEmployee(Model model){
+        model.addAttribute("deplist",departmentService.getAllDept());
         return "EmployeeForm";
     }
     @PostMapping("/employee")
     public String postEmployee(@ModelAttribute Employee employee){
         employeeService.addEmployee(employee);
-        return "EmployeeForm";
+        return "redirect:/employee";
     }
+    @GetMapping("/employeelist")
+    public String getEmployeeList(@ModelAttribute Employee employee, Model model){
+        model.addAttribute("elist",employeeService.getAllEmployee());
+//        model.addAttribute("dlist",departmentService.getAllDept());
+        return "EmployeeList";
+    }
+
 }
